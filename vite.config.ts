8d569f850e1a93c4278594b0f44181d4e826ecef
Manish Kumar Verma <manish6203993@gmail.com>
2025-05-23 08:58:09 +0530

    add delploy command

--- vite.config.ts
+++ vite.config.ts
@@ -1,6 +1,6 @@
 import { defineConfig } from "vite";
 import tailwindcss from "@tailwindcss/vite";
 export default defineConfig({
-  base: "/tailwind-login-form/",
+  base: "/tailwind-form-assignment/",
   plugins: [tailwindcss()],
 });
